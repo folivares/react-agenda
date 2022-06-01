8c4145f082d48df2386f81d33eb91cff0ef9426c
folivares <federico.olivares.dev@gmail.com>
2022-06-01 06:15:05 +0000

    Deploying to gh-pages from @ folivares/react-agenda@4ef1736085b8ae806b6b0dc76f4a25832eefa799 🚀

--- src/components/AgendaContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const AgendaContext = createContext();
-
-export default AgendaContext;
--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Agenda } from './Agenda'
-export { default as AgendaContext } from './AgendaContext'
